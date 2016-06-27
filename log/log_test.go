9ee0297a63b2b7aba5f3389006890d42fd7c0753
1313475@qq.com <1313475@qq.com>
2016-06-27 08:58:31 +0800

    创建日志模块

---
+++ log/log_test.go
@@ -0,0 +1,14 @@
+package log
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func Test(t *testing.T) {
+	Convey("日志模块", t, func() {
+		So(1, ShouldEqual, 1)
+		So(2, ShouldEqual, 2)
+	})
+}
